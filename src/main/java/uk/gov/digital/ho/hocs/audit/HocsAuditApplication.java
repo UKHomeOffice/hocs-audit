6bf717a34f0a95cd1b59100973c914545e555a35
Stuart <stuart.whitlam@digital.homeoffice.gov.uk>
2019-03-22 16:07:30 +0000

    attempt to fix silent startup failure

--- src/main/java/uk/gov/digital/ho/hocs/audit/HocsAuditApplication.java
+++ src/main/java/uk/gov/digital/ho/hocs/audit/HocsAuditApplication.java
@@ -7,7 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HocsAuditApplication {
 
 	public static void main(String[] args) {
+
+		try {
 			SpringApplication.run(HocsAuditApplication.class, args);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
