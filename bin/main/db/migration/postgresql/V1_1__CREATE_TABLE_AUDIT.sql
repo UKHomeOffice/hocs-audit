d69fb7df36e9bc0fc6b59ddd4cd87e164b4063b2
Ryan Chidwick <ryanchidwick@hod117622.local>
2022-04-06 14:01:50 +0100

    HOCS-4862:update match label action version
    Update the match label action version to the latest release.

---
+++ bin/main/db/migration/postgresql/V1_1__CREATE_TABLE_AUDIT.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS audit_data cascade;
+
+CREATE TABLE IF NOT EXISTS audit_data
+(
+  id                     BIGSERIAL   PRIMARY KEY,
+  uuid                   UUID        NOT NULL,
+  case_uuid              UUID,
+  stage_uuid             UUID,
+  correlation_id         TEXT        NOT NULL,
+  raising_service        TEXT        NOT NULL,
+  audit_payload          JSONB,
+  namespace              TEXT        NOT NULL,
+  audit_timestamp        TIMESTAMP   NOT NULL,
+  type                   TEXT        NOT NULL,
+  user_id                TEXT        NOT NULL,
+
+
+  CONSTRAINT audit_uuid_idempotent UNIQUE (uuid)
+);
+
+CREATE INDEX idx_case_uuid ON audit_data (case_uuid);
