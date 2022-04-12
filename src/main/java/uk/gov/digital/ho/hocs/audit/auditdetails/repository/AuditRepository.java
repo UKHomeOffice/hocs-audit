3f8ee1c7faa55dd029a04dc5e8113ff56f6e74d8
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2022-04-12 22:06:57 +0100

    HOCS-4940: Remove Query_Hints

--- src/main/java/uk/gov/digital/ho/hocs/audit/auditdetails/repository/AuditRepository.java
+++ src/main/java/uk/gov/digital/ho/hocs/audit/auditdetails/repository/AuditRepository.java
@@ -2,18 +2,14 @@ package uk.gov.digital.ho.hocs.audit.auditdetails.repository;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.digital.ho.hocs.audit.auditdetails.model.AuditData;
-import javax.persistence.QueryHint;
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Stream;
 
-import static org.hibernate.jpa.QueryHints.HINT_FETCH_SIZE;
 
 @Repository
 public interface AuditRepository extends PagingAndSortingRepository<AuditData, String>, AuditRepositoryCustom {
@@ -33,23 +29,18 @@ public interface AuditRepository extends PagingAndSortingRepository<AuditData, S
     @Query(value = "SELECT a.* FROM audit_data a WHERE a.audit_timestamp > ?1 AND a.deleted = false", nativeQuery = true)
     List<AuditData> findAuditData(LocalDateTime dateTime, Pageable pageRequest);
 
-    @QueryHints(value = @QueryHint(name = HINT_FETCH_SIZE, value = "50"))
     @Query(value = "SELECT a.* FROM audit_data a WHERE a.audit_timestamp > ?1 AND a.audit_timestamp < ?2 AND a.deleted = false", nativeQuery = true)
     Stream<AuditData> findAuditDataByDateRange(LocalDateTime dateFrom, LocalDateTime dateTo, Pageable pageRequest);
 
     @Query(value = "SELECT a.* FROM audit_data a WHERE a.case_uuid = ?1 AND a.type IN ?2 AND a.deleted = false", nativeQuery = true)
     List<AuditData> findAuditDataByCaseUUIDAndTypesIn(UUID caseUUID, String[] types);
 
-    @QueryHints(value = @QueryHint(name = HINT_FETCH_SIZE, value = "50"))
     @Query(value = "SELECT a.* FROM audit_data a WHERE a.audit_timestamp BETWEEN ?1 AND ?2 AND a.type in ?3 AND a.case_type = ?4 AND a.deleted = false ORDER BY a.audit_timestamp ASC", nativeQuery = true)
     Stream<AuditData> findAuditDataByDateRangeAndEvents(LocalDateTime dateFrom, LocalDateTime dateTo, String[] types, String caseType);
 
-    @QueryHints(value = @QueryHint(name = HINT_FETCH_SIZE, value = "50"))
     @Query(value = "SELECT DISTINCT ON (case_uuid, type) a.* FROM audit_data a WHERE a.audit_timestamp BETWEEN ?1 AND ?2 AND a.type in ?3 AND a.case_type = ?4 AND a.deleted = false ORDER BY a.case_uuid, a.type, a.audit_timestamp DESC;", nativeQuery = true)
     Stream<AuditData> findLastAuditDataByDateRangeAndEvents(LocalDateTime dateFrom, LocalDateTime dateTo, String[] types, String caseType);
 
     @Query(value = "SELECT a.* FROM audit_data a WHERE a.case_uuid = ?1", nativeQuery = true)
     List<AuditData> findAuditDataByCaseUUID(UUID caseUUID);
 }
-
-
