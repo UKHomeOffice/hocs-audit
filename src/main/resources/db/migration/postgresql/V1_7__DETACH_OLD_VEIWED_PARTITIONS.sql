d66a78150f1850293fdca5f7977a6d4ff4a7e063
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2022-04-13 19:18:14 +0100

    HOCS-4940: Remove (but retain) *_viewed messages as they aren't used in case timelines or extracts (#207)

---
+++ src/main/resources/db/migration/postgresql/V1_7__DETACH_OLD_VEIWED_PARTITIONS.sql
@@ -0,0 +1,19 @@
+ALTER TABLE audit_event_2018 DETACH PARTITION audit_event_2018_viewed;
+
+ALTER TABLE audit_event_2019 DETACH PARTITION audit_event_2019_viewed;
+
+ALTER TABLE audit_event_2020 DETACH PARTITION audit_event_2020_viewed;
+
+ALTER TABLE audit_event_2021_1 DETACH PARTITION audit_event_2021_1_viewed;
+
+ALTER TABLE audit_event_2021_2 DETACH PARTITION audit_event_2021_2_viewed;
+
+ALTER TABLE audit_event_2021_3 DETACH PARTITION audit_event_2021_3_viewed;
+
+ALTER TABLE audit_event_2021_4 DETACH PARTITION audit_event_2021_4_viewed;
+
+ALTER TABLE audit_event_2022_1 DETACH PARTITION audit_event_2022_1_viewed;
+
+ALTER TABLE audit_event_2022_2 DETACH PARTITION audit_event_2022_2_viewed;
+
+ALTER TABLE audit_event_2022_3 DETACH PARTITION audit_event_2022_3_viewed;
