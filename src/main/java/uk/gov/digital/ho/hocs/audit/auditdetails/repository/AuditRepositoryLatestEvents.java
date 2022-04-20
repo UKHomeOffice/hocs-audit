3d632bf33ae22039d5ff16686150334f2d45c768
Liam MacPherson <liammacpherson1996@gmail.com>
2022-04-20 09:44:53 +0100

    HOCS-4940: introduce new latest table
    Introduce a new partitioned table that holds the latest value for
    specified types. These partitions mimic the existing partitions on the
    `audit_event` table.

---
+++ src/main/java/uk/gov/digital/ho/hocs/audit/auditdetails/repository/AuditRepositoryLatestEvents.java
@@ -0,0 +1,16 @@
+package uk.gov.digital.ho.hocs.audit.auditdetails.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import uk.gov.digital.ho.hocs.audit.auditdetails.model.AuditEvent;
+
+import java.time.LocalDateTime;
+import java.util.stream.Stream;
+
+@Repository
+public interface AuditRepositoryLatestEvents {
+
+    @Query(value = "SELECT a.* FROM audit_event_latest_events a WHERE a.audit_timestamp >= ?1 AND a.type in ?2 AND a.case_type = ?3 AND a.deleted = false ORDER BY a.case_uuid, a.type, a.audit_timestamp DESC;", nativeQuery = true)
+    Stream<AuditEvent> findAuditEventLatestEventsAfterDate(LocalDateTime of, String[] events, String caseType);
+
+}
