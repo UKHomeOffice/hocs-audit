735a559dd92df98e46bf2cc8b0a6582279646c90
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2022-04-26 11:42:57 +0100

    HOCS-4120 Reduce Debug and Info logging around the Clients.

--- src/main/java/uk/gov/digital/ho/hocs/audit/core/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/uk/gov/digital/ho/hocs/audit/core/exception/RestResponseEntityExceptionHandler.java
@@ -5,8 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.RestClientException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import static net.logstash.logback.argument.StructuredArguments.value;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static uk.gov.digital.ho.hocs.audit.core.LogEvent.AUDIT_EVENT_CREATION_FAILED;
@@ -23,31 +25,37 @@ public class RestResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntityCreationException.class)
     public ResponseEntity<String> handle(EntityCreationException e) {
-        log.error("EntityCreationException", value(EVENT, AUDIT_EVENT_CREATION_FAILED), value(EXCEPTION, e));
+        log.error("EntityCreationException", value(EVENT, AUDIT_EVENT_CREATION_FAILED), value(EXCEPTION, e.toString()));
         return new ResponseEntity<>(e.getMessage(), INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<String> handle(EntityNotFoundException e) {
-        log.error("EntityNotFoundException", value(EVENT, AUDIT_RECORD_NOT_FOUND), value(EXCEPTION, e));
+        log.error("EntityNotFoundException", value(EVENT, AUDIT_RECORD_NOT_FOUND), value(EXCEPTION, e.toString()));
         return new ResponseEntity<>(e.getMessage(), NOT_FOUND);
     }
 
     @ExceptionHandler(AuditExportException.class)
     public ResponseEntity<String> handle(AuditExportException e) {
-        log.error("AuditExportException", value(EVENT, CSV_EXPORT_FAILURE), value(EXCEPTION, e));
+        log.error("AuditExportException", value(EVENT, CSV_EXPORT_FAILURE), value(EXCEPTION, e.toString()));
         return new ResponseEntity<>(e.getMessage(), NOT_FOUND);
     }
 
     @ExceptionHandler(RestClientException.class)
     public ResponseEntity<String> handle(RestClientException e) {
-        log.error("RestClientException", value(EVENT, REST_CLIENT_EXCEPTION), value(EXCEPTION, e));
+        log.error("RestClientException", value(EVENT, REST_CLIENT_EXCEPTION), value(EXCEPTION, e.toString()));
         return new ResponseEntity<>(e.getMessage(), INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> handle(MethodArgumentTypeMismatchException e) {
+        log.error("MethodArgumentTypeMismatchException", value(EVENT, REST_CLIENT_EXCEPTION), value(EXCEPTION, e.toString()));
+        return new ResponseEntity<>(e.getMessage(), BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handle(Exception e) {
-        log.error("Exception", value(EVENT, UNCAUGHT_EXCEPTION), value(EXCEPTION, e));
+        log.error("Uncaught Exception: " + e.getClass().getName(), value(EVENT, UNCAUGHT_EXCEPTION), value(EXCEPTION, e.toString()));
         return new ResponseEntity<>(e.getMessage(), INTERNAL_SERVER_ERROR);
     }
 
