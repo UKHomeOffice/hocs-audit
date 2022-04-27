5ce5663f1860635e0464c0d1cb55876cb6a97957
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2022-04-27 16:00:29 +0100

    HOCS-4980: Remove specific AuditRepositoryLatestEvents repo

--- src/main/java/uk/gov/digital/ho/hocs/audit/repository/AuditRepository.java
+++ src/main/java/uk/gov/digital/ho/hocs/audit/repository/AuditRepository.java
@@ -18,7 +18,7 @@ import static org.hibernate.jpa.QueryHints.HINT_CACHEABLE;
 import static org.hibernate.jpa.QueryHints.HINT_FETCH_SIZE;
 
 @Repository
-public interface AuditRepository extends CrudRepository<AuditEvent, String>, AuditRepositoryCustom, AuditRepositoryLatestEvents {
+public interface AuditRepository extends CrudRepository<AuditEvent, String>, AuditRepositoryCustom {
 
     @QueryHints(value = {
             @QueryHint(name = HINT_FETCH_SIZE, value = "1"),
@@ -36,13 +36,17 @@ public interface AuditRepository extends CrudRepository<AuditEvent, String>, Aud
     @Query(value = "SELECT a.* FROM audit_event a WHERE a.audit_timestamp BETWEEN ?3 AND 'tomorrow' AND a.case_uuid = ?1 AND a.type IN ?2 AND a.deleted = false", nativeQuery = true)
     Stream<AuditEvent> findAuditDataByCaseUUIDAndTypesInAndFrom(UUID caseUUID, String[] types, LocalDate from);
 
+    @Modifying
+    @Query(value = "UPDATE audit_event a SET a.deleted = ?2  WHERE a.case_uuid = ?1", nativeQuery = true)
+    int updateAuditDataDeleted(UUID caseUUID, boolean deleted);
+
     @Query(value = "SELECT a.* FROM audit_event a WHERE a.audit_timestamp BETWEEN ?1 AND ?2 AND a.type in ?3 AND a.case_type = ?4 AND a.deleted = false ORDER BY a.audit_timestamp ASC", nativeQuery = true)
     Stream<AuditEvent> findAuditDataByDateRangeAndEvents(LocalDateTime dateFrom, LocalDateTime dateTo, String[] types, String caseType);
 
     @Query(value = "SELECT DISTINCT ON (case_uuid, type) a.* FROM audit_event a WHERE a.audit_timestamp BETWEEN ?1 AND ?2 AND a.type in ?3 AND a.case_type = ?4 AND a.deleted = false ORDER BY a.case_uuid, a.type, a.audit_timestamp DESC;", nativeQuery = true)
     Stream<AuditEvent> findLastAuditDataByDateRangeAndEvents(LocalDateTime dateFrom, LocalDateTime dateTo, String[] types, String caseType);
 
-    @Modifying
-    @Query(value = "UPDATE audit_event a SET a.deleted = ?2  WHERE a.case_uuid = ?1", nativeQuery = true)
-    int updateAuditDataDeleted(UUID caseUUID, boolean deleted);
+    @Query(value = "SELECT a.* FROM audit_event_latest_events a WHERE a.audit_timestamp between ?1 and 'tomorrow' AND a.type in ?2 AND a.case_type = ?3 AND a.deleted = false ORDER BY a.case_uuid, a.type, a.audit_timestamp DESC", nativeQuery = true)
+    Stream<AuditEvent> findAuditEventLatestEventsAfterDate(LocalDateTime of, String[] events, String caseType);
+
 }
