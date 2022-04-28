f0dc2d7175c416fb94b2fd70e02ad9bafaf1cc55
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2022-04-28 21:54:24 +0100

    HOCS-4980: Stream the audit timeline data

--- src/main/java/uk/gov/digital/ho/hocs/audit/repository/AuditRepository.java
+++ src/main/java/uk/gov/digital/ho/hocs/audit/repository/AuditRepository.java
@@ -36,9 +36,19 @@ public interface AuditRepository extends CrudRepository<AuditEvent, String>, Aud
     @Query(value = "SELECT a.* FROM audit_event a WHERE a.audit_timestamp BETWEEN ?3 AND 'tomorrow' AND a.case_uuid = ?1 AND a.type IN ?2 AND a.deleted = false", nativeQuery = true)
     Stream<AuditEvent> findAuditDataByCaseUUIDAndTypesInAndFrom(UUID caseUUID, String[] types, LocalDate from);
 
+    @QueryHints(value = {
+            @QueryHint(name = HINT_FETCH_SIZE, value = "1"),
+            @QueryHint(name = HINT_CACHEABLE, value = "false"),
+            @QueryHint(name = READ_ONLY, value = "true")
+    })
     @Query(value = "SELECT a.* FROM audit_event a WHERE a.audit_timestamp BETWEEN ?1 AND ?2 AND a.type in ?3 AND a.case_type = ?4 AND a.deleted = false ORDER BY a.audit_timestamp ASC", nativeQuery = true)
     Stream<AuditEvent> findAuditDataByDateRangeAndEvents(LocalDateTime dateFrom, LocalDateTime dateTo, String[] types, String caseType);
 
+    @QueryHints(value = {
+            @QueryHint(name = HINT_FETCH_SIZE, value = "1"),
+            @QueryHint(name = HINT_CACHEABLE, value = "false"),
+            @QueryHint(name = READ_ONLY, value = "true")
+    })
     @Query(value = "SELECT DISTINCT ON (case_uuid, type) a.* FROM audit_event a WHERE a.audit_timestamp BETWEEN ?1 AND ?2 AND a.type in ?3 AND a.case_type = ?4 AND a.deleted = false ORDER BY a.case_uuid, a.type, a.audit_timestamp DESC;", nativeQuery = true)
     Stream<AuditEvent> findLastAuditDataByDateRangeAndEvents(LocalDateTime dateFrom, LocalDateTime dateTo, String[] types, String caseType);
 
