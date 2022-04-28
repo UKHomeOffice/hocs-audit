b6f0b2ccba35fb002507a0ccf59b3765c54d0011
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2022-04-28 11:35:15 +0100

    HOCS-4980: pass the case_uuid as a string

--- src/main/java/uk/gov/digital/ho/hocs/audit/repository/AuditRepositoryLatestEvents.java
+++ src/main/java/uk/gov/digital/ho/hocs/audit/repository/AuditRepositoryLatestEvents.java
@@ -14,7 +14,7 @@ public interface AuditRepositoryLatestEvents {
     @Query(value = "SELECT a.* FROM audit_event_latest_events a WHERE a.audit_timestamp between ?1 and 'tomorrow' AND a.type in ?2 AND a.case_type = ?3 AND a.deleted = false ORDER BY a.case_uuid, a.type, a.audit_timestamp DESC", nativeQuery = true)
     Stream<AuditEvent> findAuditEventLatestEventsAfterDate(LocalDateTime of, String[] events, String caseType);
 
-    @Query(value = "SELECT audit_payload->>'reference' AS caseReference, case_uuid::TEXT as caseUuid FROM audit_event_latest_events WHERE type = 'CASE_CREATED' AND case_type = ?1", nativeQuery = true)
+    @Query(value = "SELECT audit_payload->>'reference' AS caseReference, CAST(case_uuid AS VARCHAR(36)) as caseUuid FROM audit_event_latest_events WHERE type = 'CASE_CREATED' AND case_type = ?1", nativeQuery = true)
     Stream<CaseReference> getCaseReferencesForType(String caseType);
 
 }
