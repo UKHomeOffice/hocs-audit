3ae86583811a998f3c8960bf3867d58dcef7391c
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2022-04-28 22:58:43 +0100

    HOCS-4980: try different stream value (can be any positive integer)

--- src/main/java/uk/gov/digital/ho/hocs/audit/repository/AuditRepository.java
+++ src/main/java/uk/gov/digital/ho/hocs/audit/repository/AuditRepository.java
@@ -1,10 +1,11 @@
 package uk.gov.digital.ho.hocs.audit.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.QueryHints;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.digital.ho.hocs.audit.repository.entity.AuditEvent;
+import uk.gov.digital.ho.hocs.audit.repository.entity.CaseReference;
 
 import javax.persistence.QueryHint;
 import java.time.LocalDate;
@@ -18,10 +19,10 @@ import static org.hibernate.jpa.QueryHints.HINT_CACHEABLE;
 import static org.hibernate.jpa.QueryHints.HINT_FETCH_SIZE;
 
 @Repository
-public interface AuditRepository extends CrudRepository<AuditEvent, String>, AuditRepositoryCustom, AuditRepositoryLatestEvents {
+public interface AuditRepository extends JpaRepository<AuditEvent, String>, AuditRepositoryCustom {
 
     @QueryHints(value = {
-            @QueryHint(name = HINT_FETCH_SIZE, value = "1000"),
+            @QueryHint(name = HINT_FETCH_SIZE, value = "10000"),
             @QueryHint(name = HINT_CACHEABLE, value = "false"),
             @QueryHint(name = READ_ONLY, value = "true")
     })
@@ -29,7 +30,7 @@ public interface AuditRepository extends CrudRepository<AuditEvent, String>, Aud
     Stream<AuditEvent> findAuditDataByCaseUUIDAndTypesIn(UUID caseUUID, String[] types);
 
     @QueryHints(value = {
-            @QueryHint(name = HINT_FETCH_SIZE, value = "1000"),
+            @QueryHint(name = HINT_FETCH_SIZE, value = "10000"),
             @QueryHint(name = HINT_CACHEABLE, value = "false"),
             @QueryHint(name = READ_ONLY, value = "true")
     })
@@ -37,7 +38,7 @@ public interface AuditRepository extends CrudRepository<AuditEvent, String>, Aud
     Stream<AuditEvent> findAuditDataByCaseUUIDAndTypesInAndFrom(UUID caseUUID, String[] types, LocalDate from);
 
     @QueryHints(value = {
-            @QueryHint(name = HINT_FETCH_SIZE, value = "1000"),
+            @QueryHint(name = HINT_FETCH_SIZE, value = "10000"),
             @QueryHint(name = HINT_CACHEABLE, value = "false"),
             @QueryHint(name = READ_ONLY, value = "true")
     })
@@ -45,7 +46,7 @@ public interface AuditRepository extends CrudRepository<AuditEvent, String>, Aud
     Stream<AuditEvent> findAuditDataByDateRangeAndEvents(LocalDateTime dateFrom, LocalDateTime dateTo, String[] types, String caseType);
 
     @QueryHints(value = {
-            @QueryHint(name = HINT_FETCH_SIZE, value = "1000"),
+            @QueryHint(name = HINT_FETCH_SIZE, value = "10000"),
             @QueryHint(name = HINT_CACHEABLE, value = "false"),
             @QueryHint(name = READ_ONLY, value = "true")
     })
@@ -54,4 +55,20 @@ public interface AuditRepository extends CrudRepository<AuditEvent, String>, Aud
 
     @Query(value = "SELECT a.* FROM audit_event a WHERE a.case_uuid = ?1", nativeQuery = true)
     List<AuditEvent> findAuditDataByCaseUUID(UUID caseUUID);
+
+    @QueryHints(value = {
+            @QueryHint(name = HINT_FETCH_SIZE, value = "10000"),
+            @QueryHint(name = HINT_CACHEABLE, value = "false"),
+            @QueryHint(name = READ_ONLY, value = "true")
+    })
+    @Query(value = "SELECT a.* FROM audit_event_latest_events a WHERE a.audit_timestamp between ?1 and 'tomorrow' AND a.type in ?2 AND a.case_type = ?3 AND a.deleted = false ORDER BY a.case_uuid, a.type, a.audit_timestamp DESC", nativeQuery = true)
+    Stream<AuditEvent> findAuditEventLatestEventsAfterDate(LocalDateTime of, String[] events, String caseType);
+
+    @QueryHints(value = {
+            @QueryHint(name = HINT_FETCH_SIZE, value = "10000"),
+            @QueryHint(name = HINT_CACHEABLE, value = "false"),
+            @QueryHint(name = READ_ONLY, value = "true")
+    })
+    @Query(value = "SELECT audit_payload->>'reference' AS caseReference, cast(case_uuid AS VARCHAR(36)) as caseUuid FROM audit_event_latest_events WHERE type = 'CASE_CREATED' AND case_type = ?1", nativeQuery = true)
+    Stream<CaseReference> getCaseReferencesForType(String caseType);
 }
