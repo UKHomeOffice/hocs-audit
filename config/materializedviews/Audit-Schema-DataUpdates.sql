0a8fb949ab557c929efafb92eecb4062d983e058
Liam MacPherson <liammacpherson1996@gmail.com>
2022-05-26 14:16:05 +0100

    HOCS-5126: rework materialized view
    This change reworks the creation of these views to minimize any impact
    changes to this view have on users if applied in live environments.

--- config/materializedviews/Audit-Schema-DataUpdates.sql
+++ config/materializedviews/Audit-Schema-DataUpdates.sql
@@ -1,9 +1,6 @@
 SET search_path TO audit;
 
-DROP MATERIALIZED VIEW IF EXISTS DCU_AGGREGATED_CASES CASCADE;
-
-CREATE MATERIALIZED VIEW DCU_AGGREGATED_CASES AS
-
+CREATE MATERIALIZED VIEW DCU_AGGREGATED_CASES_TEMP AS
 WITH CTE_Correspondents AS (
     SELECT * FROM (
                       SELECT
@@ -47,7 +44,6 @@ WITH CTE_Correspondents AS (
            AND NOT deleted
          GROUP BY case_uuid
      )
-
 SELECT
     grouped.*,
     COALESCE(commentCounts."commentCount", 0) AS "commentCount",
@@ -72,10 +68,9 @@ SELECT
     CASE WHEN pc."correspondentType" = 'MEMBER' THEN pc."externalKey" ELSE NULL END AS "mpRef",
     CASE WHEN pc."correspondentType" = 'MEMBER' THEN pc."fullname" ELSE NULL END AS "member",
     now()::timestamp(0) AS last_refresh
-
 FROM (
-     SELECT
-         case_uuid::text as "caseUuid",
+         SELECT
+             case_uuid::text as "caseUuid",
              MAX(deadline) FILTER( WHERE stageName LIKE '%_MARKUP' AND ranked.type = 'STAGE_CREATED' AND Last_Of_Stage_Event_RK = 1) AS "markupDeadline",
              MAX(audit_timestamp) FILTER( WHERE stageName LIKE '%_INITIAL_DRAFT' AND ranked.type = 'STAGE_RECREATED' AND Last_Of_Stage_Event_RK = 1) AS "reDraftStarted",
              MAX(allocatedToUUID) FILTER( WHERE Last_dataTypeChange_Of_Case_RK = 1 AND dataTypeChange = 'TEAM') AS "assignedTeam",
@@ -135,100 +130,99 @@ FROM (
              MAX(overridePOTeamUnitHistoricName) FILTER( WHERE dataTypeChange = 'CASE_DATA' AND Last_dataTypeChange_Of_Case_RK = 1) AS "overridePOTeamUnitHistoricName",
              MAX(privateOfficeOverridePOTeamUnitHistoricName) FILTER( WHERE dataTypeChange = 'CASE_DATA' AND Last_dataTypeChange_Of_Case_RK = 1) AS "privateOfficeOverridePOTeamUnitHistoricName",
              MAX(defaultPolicyTeamUnitHistoricName) FILTER( WHERE dataTypeChange = 'CASE_DATA' AND Last_dataTypeChange_Of_Case_RK = 1) AS "defaultPolicyTeamUnitHistoricName"
-
          FROM (
-                 SELECT
-                     to_char(audit_timestamp, 'YYYY-MM-DD"T"HH24:MI:SS.US') AS audit_timestamp,
-                     ad.case_uuid,
-                     dataTypeChange,
-                     "type",
-                     user_id,
-                     RANK() OVER (
-                         PARTITION BY ad.case_uuid, stage_uuid, ad.type
-                         ORDER BY ad.audit_timestamp DESC, ad.id DESC
-                         ) AS Last_Of_Stage_Event_RK,
-                         RANK() OVER (
-                         PARTITION BY ad.case_uuid, stage_uuid, ad.type
-                         ORDER BY ad.audit_timestamp ASC, ad.id DESC
-                         ) AS First_Of_Stage_Event_RK,
-                         RANK() OVER (
-                         PARTITION BY ad.case_uuid, ad.type
-                         ORDER BY ad.audit_timestamp DESC, ad.id DESC
-                         ) AS Last_Of_Event_RK,
-                         RANK() OVER (
-                         PARTITION BY ad.case_uuid
-                         ORDER BY CASE WHEN audit_payload::jsonb->>'stage' IS NULL THEN 0 ELSE 1 END DESC, ad.audit_timestamp DESC, ad.id DESC
-                         ) AS Last_Stage_Event_Of_Case_RK,
-                         RANK() OVER (
-                         PARTITION BY ad.case_uuid
-                         ORDER BY ad.audit_timestamp DESC, ad.id DESC
-                         ) AS Last_Event_Of_Case_RK,
-                         RANK() OVER (
-                         PARTITION BY ad.case_uuid, dataTypeChange
-                         ORDER BY ad.audit_timestamp DESC, ad.id DESC
-                         ) AS Last_dataTypeChange_Of_Case_RK,
-                         RANK() OVER (
-                         PARTITION BY ad.case_uuid, stage_uuid, dataTypeChange
-                         ORDER BY ad.audit_timestamp DESC, ad.id DESC
-                         ) AS Last_dataTypeChange_Of_Stage_RK,
-                         audit_payload::jsonb->>'stage' AS stageName,
-                     audit_payload::jsonb->>'allocatedToUUID' AS allocatedToUUID,
-                     audit_payload::jsonb->>'deadline' AS deadline,
-                     audit_payload::jsonb->>'reference' AS reference,
-                     audit_payload::jsonb->>'caseDeadline' AS caseDeadline,
-                     audit_payload::jsonb->'data'->>'OriginalChannel' AS originalChannel,
-                     audit_payload::jsonb->>'type' AS payloadType,
-                     audit_payload::jsonb->'data'->>'DateOfCorrespondence' AS dateOfCorrespondence,
-                     audit_payload::jsonb->'data'->>'DateReceived' AS dateReceived,
-                     audit_payload::jsonb->'data'->>'MarkupDecision' AS markupDecision,
-                     COALESCE(NULLIF(audit_payload::jsonb->'data'->>'OverrideDraftingTeamUUID', ''), NULLIF(audit_payload::jsonb->'data'->>'DraftingTeamUUID', '')) AS draftingTeamUUID,
-                     COALESCE(NULLIF(audit_payload::jsonb->'data'->>'PrivateOfficeOverridePOTeamUUID', ''), NULLIF(audit_payload::jsonb->'data'->>'OverridePOTeamUUID', ''), NULLIF(audit_payload::jsonb->'data'->>'POTeamUUID', '')) AS privateOfficeTeamUUID,
-                     audit_payload::jsonb->>'primaryTopic' AS primaryTopic,
-                     audit_payload::jsonb->'data'->>'NoReplyNeededConfirmation' AS "noReplyNeededConfirmation",
-                     audit_payload::jsonb->'data'->>'TransferConfirmation' AS "transferConfirmation",
-                     audit_payload::jsonb->'data'->>'OGDDept' AS "ogdDept",
-                     LOWER(audit_payload::jsonb->'data'->>'CopyNumberTen') AS "copyNumberTen",
-                     audit_payload::jsonb->'data'->>'MinisterSignOffDecision' AS "ministerSignOffDecision",
-                     audit_payload::jsonb->>'primaryCorrespondent' as "primaryCorrespondentUuid",
-                     audit_payload::jsonb->'data'->>'DefaultPolicyTeamUUID' AS defaultPolicyTeamUuid,
-                     audit_payload::jsonb->'data'->>'DraftCount' AS draftCount,
-                     audit_payload::jsonb->'data'->>'QAOnlineCount' AS qaOnlineCount,
-                     audit_payload::jsonb->'data'->>'HomeSecInterest' AS homeSecInterest,
-                     audit_payload::jsonb->'data'->>'HomeSecReply' AS homeSecReply,
-                     audit_payload::jsonb->'data'->>'DraftingTeamUnitHistoricName' AS draftingTeamUnitHistoricName, -- add historic unit name fields
-                     audit_payload::jsonb->'data'->>'POTeamUnitHistoricName' AS poTeamUnitHistoricName,
-                     audit_payload::jsonb->'data'->>'OverrideDraftingTeamUnitHistoricName' AS overrideDraftingTeamUnitHistoricName,
-                     audit_payload::jsonb->'data'->>'OverridePOTeamUnitHistoricName' AS overridePOTeamUnitHistoricName,
-                     audit_payload::jsonb->'data'->>'PrivateOfficeOverridePOTeamUnitHistoricName' AS privateOfficeOverridePOTeamUnitHistoricName,
-                     audit_payload::jsonb->'data'->>'DefaultPolicyTeamUnitHistoricName' AS defaultPolicyTeamUnitHistoricName
-                 FROM (
-                     SELECT
-                     audit_payload,
-                     audit_timestamp,
-                     case_uuid,
-                     "id",
-                     stage_uuid,
-                     "type",
-                     user_id,
-                     CASE WHEN "type" IN ('CASE_CREATED', 'CASE_UPDATED') THEN 'CASE_DATA'
-                     WHEN "type" IN ('STAGE_ALLOCATED_TO_USER', 'STAGE_UNALLOCATED_FROM_USER') THEN 'USER'
-                     WHEN "type" IN ('STAGE_CREATED', 'STAGE_ALLOCATED_TO_TEAM') THEN 'TEAM'
-                     END AS dataTypeChange
-                     FROM audit_event
-                     WHERE case_type IN ('a1', 'a2', 'a3')
-                     AND "type" IN ('CASE_COMPLETED',
-                     'CASE_CREATED',
-                     'CASE_UPDATED',
-                     'STAGE_ALLOCATED_TO_TEAM',
-                     'STAGE_ALLOCATED_TO_USER',
-                     'STAGE_COMPLETED',
-                     'STAGE_CREATED',
-                     'STAGE_RECREATED',
-                     'STAGE_UNALLOCATED_FROM_USER')
-                     AND NOT deleted
-                     ) ad
-                 ORDER BY audit_timestamp DESC
-             ) ranked
+                  SELECT
+                      to_char(audit_timestamp, 'YYYY-MM-DD"T"HH24:MI:SS.US') AS audit_timestamp,
+                      ad.case_uuid,
+                      dataTypeChange,
+                      "type",
+                      user_id,
+                      RANK() OVER (
+                          PARTITION BY ad.case_uuid, stage_uuid, ad.type
+                          ORDER BY ad.audit_timestamp DESC, ad.id DESC
+                          ) AS Last_Of_Stage_Event_RK,
+                      RANK() OVER (
+                          PARTITION BY ad.case_uuid, stage_uuid, ad.type
+                          ORDER BY ad.audit_timestamp ASC, ad.id DESC
+                          ) AS First_Of_Stage_Event_RK,
+                      RANK() OVER (
+                          PARTITION BY ad.case_uuid, ad.type
+                          ORDER BY ad.audit_timestamp DESC, ad.id DESC
+                          ) AS Last_Of_Event_RK,
+                      RANK() OVER (
+                          PARTITION BY ad.case_uuid
+                          ORDER BY CASE WHEN audit_payload::jsonb->>'stage' IS NULL THEN 0 ELSE 1 END DESC, ad.audit_timestamp DESC, ad.id DESC
+                          ) AS Last_Stage_Event_Of_Case_RK,
+                      RANK() OVER (
+                          PARTITION BY ad.case_uuid
+                          ORDER BY ad.audit_timestamp DESC, ad.id DESC
+                          ) AS Last_Event_Of_Case_RK,
+                      RANK() OVER (
+                          PARTITION BY ad.case_uuid, dataTypeChange
+                          ORDER BY ad.audit_timestamp DESC, ad.id DESC
+                          ) AS Last_dataTypeChange_Of_Case_RK,
+                      RANK() OVER (
+                          PARTITION BY ad.case_uuid, stage_uuid, dataTypeChange
+                          ORDER BY ad.audit_timestamp DESC, ad.id DESC
+                          ) AS Last_dataTypeChange_Of_Stage_RK,
+                      audit_payload::jsonb->>'stage' AS stageName,
+                      audit_payload::jsonb->>'allocatedToUUID' AS allocatedToUUID,
+                      audit_payload::jsonb->>'deadline' AS deadline,
+                      audit_payload::jsonb->>'reference' AS reference,
+                      audit_payload::jsonb->>'caseDeadline' AS caseDeadline,
+                      audit_payload::jsonb->'data'->>'OriginalChannel' AS originalChannel,
+                      audit_payload::jsonb->>'type' AS payloadType,
+                      audit_payload::jsonb->'data'->>'DateOfCorrespondence' AS dateOfCorrespondence,
+                      audit_payload::jsonb->'data'->>'DateReceived' AS dateReceived,
+                      audit_payload::jsonb->'data'->>'MarkupDecision' AS markupDecision,
+                      COALESCE(NULLIF(audit_payload::jsonb->'data'->>'OverrideDraftingTeamUUID', ''), NULLIF(audit_payload::jsonb->'data'->>'DraftingTeamUUID', '')) AS draftingTeamUUID,
+                      COALESCE(NULLIF(audit_payload::jsonb->'data'->>'PrivateOfficeOverridePOTeamUUID', ''), NULLIF(audit_payload::jsonb->'data'->>'OverridePOTeamUUID', ''), NULLIF(audit_payload::jsonb->'data'->>'POTeamUUID', '')) AS privateOfficeTeamUUID,
+                      audit_payload::jsonb->>'primaryTopic' AS primaryTopic,
+                      audit_payload::jsonb->'data'->>'NoReplyNeededConfirmation' AS "noReplyNeededConfirmation",
+                      audit_payload::jsonb->'data'->>'TransferConfirmation' AS "transferConfirmation",
+                      audit_payload::jsonb->'data'->>'OGDDept' AS "ogdDept",
+                      LOWER(audit_payload::jsonb->'data'->>'CopyNumberTen') AS "copyNumberTen",
+                      audit_payload::jsonb->'data'->>'MinisterSignOffDecision' AS "ministerSignOffDecision",
+                      audit_payload::jsonb->>'primaryCorrespondent' as "primaryCorrespondentUuid",
+                      audit_payload::jsonb->'data'->>'DefaultPolicyTeamUUID' AS defaultPolicyTeamUuid,
+                      audit_payload::jsonb->'data'->>'DraftCount' AS draftCount,
+                      audit_payload::jsonb->'data'->>'QAOnlineCount' AS qaOnlineCount,
+                      audit_payload::jsonb->'data'->>'HomeSecInterest' AS homeSecInterest,
+                      audit_payload::jsonb->'data'->>'HomeSecReply' AS homeSecReply,
+                      audit_payload::jsonb->'data'->>'DraftingTeamUnitHistoricName' AS draftingTeamUnitHistoricName, -- add historic unit name fields
+                      audit_payload::jsonb->'data'->>'POTeamUnitHistoricName' AS poTeamUnitHistoricName,
+                      audit_payload::jsonb->'data'->>'OverrideDraftingTeamUnitHistoricName' AS overrideDraftingTeamUnitHistoricName,
+                      audit_payload::jsonb->'data'->>'OverridePOTeamUnitHistoricName' AS overridePOTeamUnitHistoricName,
+                      audit_payload::jsonb->'data'->>'PrivateOfficeOverridePOTeamUnitHistoricName' AS privateOfficeOverridePOTeamUnitHistoricName,
+                      audit_payload::jsonb->'data'->>'DefaultPolicyTeamUnitHistoricName' AS defaultPolicyTeamUnitHistoricName
+                  FROM (
+                           SELECT
+                               audit_payload,
+                               audit_timestamp,
+                               case_uuid,
+                               "id",
+                               stage_uuid,
+                               "type",
+                               user_id,
+                               CASE WHEN "type" IN ('CASE_CREATED', 'CASE_UPDATED') THEN 'CASE_DATA'
+                                    WHEN "type" IN ('STAGE_ALLOCATED_TO_USER', 'STAGE_UNALLOCATED_FROM_USER') THEN 'USER'
+                                    WHEN "type" IN ('STAGE_CREATED', 'STAGE_ALLOCATED_TO_TEAM') THEN 'TEAM'
+                                   END AS dataTypeChange
+                           FROM audit_event
+                           WHERE case_type IN ('a1', 'a2', 'a3')
+                             AND "type" IN ('CASE_COMPLETED',
+                                            'CASE_CREATED',
+                                            'CASE_UPDATED',
+                                            'STAGE_ALLOCATED_TO_TEAM',
+                                            'STAGE_ALLOCATED_TO_USER',
+                                            'STAGE_COMPLETED',
+                                            'STAGE_CREATED',
+                                            'STAGE_RECREATED',
+                                            'STAGE_UNALLOCATED_FROM_USER')
+                             AND NOT deleted
+                       ) ad
+                  ORDER BY audit_timestamp DESC
+              ) ranked
          GROUP BY case_uuid
      ) grouped
          LEFT OUTER JOIN CTE_Correspondents pc
@@ -238,17 +232,27 @@ FROM (
         CTE_Correspondents cc
     WHERE cc.case_uuid = grouped."caseUuid"
       AND cc."correspondentUUID" <> grouped."primaryCorrespondentUuid"
-        LIMIT 1
+    LIMIT 1
     ) sc
-ON sc.case_uuid = grouped."caseUuid"
-    AND sc."correspondentUUID" <> grouped."primaryCorrespondentUuid"
-    LEFT OUTER JOIN CTE_CommentCounts commentCounts
-    ON commentCounts.case_uuid = grouped."caseUuid";
+                         ON sc.case_uuid = grouped."caseUuid"
+                             AND sc."correspondentUUID" <> grouped."primaryCorrespondentUuid"
+         LEFT OUTER JOIN CTE_CommentCounts commentCounts
+                         ON commentCounts.case_uuid = grouped."caseUuid"
+WITH NO DATA;
+
+CREATE UNIQUE INDEX idx_dcu_aggregated_cases_temp_case_uuid ON DCU_AGGREGATED_CASES_TEMP("caseUuid");
+
+REFRESH MATERIALIZED VIEW DCU_AGGREGATED_CASES_TEMP;
+
+DROP MATERIALIZED VIEW IF EXISTS DCU_AGGREGATED_CASES CASCADE;
+
+ALTER TABLE DCU_AGGREGATED_CASES_TEMP RENAME TO DCU_AGGREGATED_CASES;
+
+ALTER INDEX idx_dcu_aggregated_cases_temp_case_uuid RENAME TO idx_dcu_aggregated_cases_case_uuid;
 
 DROP VIEW IF EXISTS DCU_PRAU_WORKFLOW;
 
 CREATE VIEW DCU_PRAU_WORKFLOW AS
-
 SELECT
     "assignedTeam"
      ,"assignedUnit"
@@ -310,7 +314,6 @@ FROM DCU_AGGREGATED_CASES;
 DROP VIEW IF EXISTS DCU_BUSINESS;
 
 CREATE VIEW DCU_BUSINESS AS
-
 SELECT
     "assignedTeam"
      ,"assignedUnit"
